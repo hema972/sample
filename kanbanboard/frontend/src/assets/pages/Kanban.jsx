e779ca090faf12e0275ba054e1a0e3d08a50cd1c
hema972 <hema42426@email.com>
2026-03-12 16:31:00 +0530

    api changed

--- kanbanboard/frontend/src/assets/pages/Kanban.jsx
+++ kanbanboard/frontend/src/assets/pages/Kanban.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../App.css";
 
-const API = "http://localhost:5000/api/tasks";
+const API = "https://sample-1-x2zm.onrender.com/api/tasks";
 
 const Kanban = () => {
   const [todoTasks, setTodoTasks] = useState([]);
